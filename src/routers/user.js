896e6fbe84e33e5415ae8049daa49eb5f545a5da
Yash Vishwakarma <vishakarmayash24@gmail.com>
2021-12-12 14:18:54 +0530

    Bug Fixed 1

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,7 +5,6 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         const token = await user.generateAuthToken()
